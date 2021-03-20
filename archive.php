12463af4bf849c3aac2d93aaf2e270dffb19c750
MAC-gg <mc669756@gmail.com>
2021-03-20 14:43:35 -0400

    implement archive template

--- archive.php
+++ archive.php
@@ -3,9 +3,15 @@
   <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
       <div class="page-banner__content container container--narrow">
-          <h1 class="page-banner__title">Welcome to our Blog!</h1>
+          <h1 class="page-banner__title">
+            <?php // DYNAMIC NAME depending on archive type
+                if(is_category()) { single_cat_title(); echo " Posts"; }
+                else if(is_author()) { echo "Posts by "; the_author(); }
+                else { the_archive_title(); }
+            ?>
+          </h1>
           <div class="page-banner__intro">
-              <p>Keep up with our latest news!</p>
+              <p><?php the_archive_description(); // author bio, category desc, etc ?></p>
           </div>
       </div>  
   </div>
