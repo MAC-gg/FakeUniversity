0b59d2ff32e88a41d4eb90466f43781e2a4b4167
MAC-gg <mc669756@gmail.com>
2021-04-05 21:14:05 -0400

    get_template_part() for single-event-li

--- single-program.php
+++ single-program.php
@@ -62,27 +62,10 @@
                         echo '<h2 class="headline headline--medium">Related ' . get_the_title() . ' Events</h2>';
     
                         while($relatedEvents->have_posts()) {
-                            $relatedEvents->the_post(); ?>
-                            <div class="event-summary">
-                        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                            <span class="event-summary__month">
-                            <?php 
-                                $eventDate = new DateTime(get_field('event_date'));
-                                echo $eventDate->format('M');
-                            ?>
-                            </span>
-                            <span class="event-summary__day">
-                            <?php
-                                echo $eventDate->format('d');
-                            ?>
-                            </span>
-                        </a>
-                        <div class="event-summary__content">
-                            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                            <p><?php if(has_excerpt()) {echo get_the_excerpt(); } else { echo wp_trim_words(get_the_content(), 17); } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                        </div>
-                        </div>
-                    <?php } wp_reset_postdata();
+                            $relatedEvents->the_post(); 
+                        
+                            get_template_part('template-parts/single-event-li');
+                        } wp_reset_postdata();
                     } ?>
             </div>
     <?php } // END while have_posts()
